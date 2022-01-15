5b148f8a562cec649cf00d205d6362dd2eba1c4b
Grace <grace.liaw@digital-forge.net>
2022-01-15 21:47:26 +0800

    Initial Commit

---
+++ Movie/MovieApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieApp.swift
+//  Movie
+//
+//  Created by Grace Liaw on 15/1/22.
+//
+
+import SwiftUI
+
+@main
+struct MovieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
